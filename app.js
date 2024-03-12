9b93d293b928b60d8e3b4d0e16eebad66392da98
Polyhalov <apolyhalov@gmail.com>
2024-03-12 13:05:43 +0200

    done

--- app.js
+++ app.js
@@ -22,13 +22,13 @@ app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
 });
-const { DB_HOST } = process.env;
+
+const { DB_HOST, PORT=3000 } = process.env;
+
 mongoose.connect(DB_HOST)
-  .then(() => { app.listen(3000, () => console.log('Database connection successful')) })
+  .then(() => { app.listen(PORT, () => console.log('Database connection successful')) })
   .catch(error => {
     console.log(error.message);
     process.exit(1);
   })
-// app.listen(3000, () => {
-//   console.log("Server is running. Use our API on port: 3000");
-// });
+
